62c8856fd4c045cc032454b8754650b610a361b4
Pedro Manuel Quintela Ribeiro <pedro_rib2@hotmail.com>
2018-10-08 16:08:03 +0100

    Added new stuff

--- ADS122C04.ino
+++ ADS122C04.ino
@@ -21,27 +21,27 @@ void setup() {
   Serial.begin(115200); //Initialize UART communication
   Keyboard.begin();
   Wire.begin(); //Initialize I2C communication
-  #if ADS122_DEBUG==1 
+#if ADS122_DEBUG==1
   Serial.println("Arduino up from powerdown state");
-  #endif
+#endif
   adc.init(byte(ADC_ADDRESS)); //Initialize the ADS122 instance, using the I2C address as input argument
   delay(1000);
   //Configure register 0. This configuration configures the ADC to measure the voltage from channel 3 as
   //a single ended input, with gain 1 and disables the internal programmable gain amplifier (PGA)
-  adc.set(byte(ADS122_REG0),byte(ADS122_MUX_IN3|ADS122_GAIN_1|ADS122_PGA_DISABLED)); 
+  adc.set(byte(ADS122_REG0), byte(ADS122_MUX_IN3 | ADS122_GAIN_1 | ADS122_PGA_DISABLED));
   delay(100);
   //Configure register 1. This sets the acquisition speed to 2000 SPS (2x1000 SPS, because TURBO mode is on), the
   //measurement mode to single, disables the internal temperature sensor and sets the ADC voltage reference to the internal reference (2.048 V)
-  adc.set(byte(ADS122_REG1),byte(ADS122_DR_90|ADS122_MODE_TURBO|ADS122_CM_SINGLE|ADS122_VREF_INTERNAL|ADS122_TS_DISABLED));
+  adc.set(byte(ADS122_REG1), byte(ADS122_DR_90 | ADS122_MODE_TURBO | ADS122_CM_SINGLE | ADS122_VREF_INTERNAL | ADS122_TS_DISABLED));
   delay(1000);
   adc.calib();
-  #if ADS122_DEBUG == 1
+#if ADS122_DEBUG == 1
   Serial.print("X calibration value: ");
   Serial.println(adc.cal_x);
   Serial.print("Y calibration value: ");
   Serial.println(adc.cal_y);
   delay(2500);
-  #endif
+#endif
 }
 
 byte val = 0;
@@ -54,17 +54,17 @@ void loop() {
   Byte3 result;
   long int res;
 
-  adc.set(byte(ADS122_REG0),byte(ADS122_MUX_IN3|ADS122_GAIN_1|ADS122_PGA_DISABLED)); 
+  adc.set(byte(ADS122_REG0), byte(ADS122_MUX_IN3 | ADS122_GAIN_1 | ADS122_PGA_DISABLED));
 
-  if(Serial.available()){
+  if (Serial.available()) {
     lbound = Serial.parseInt();
     ubound = Serial.parseInt();
     Serial.println("New calibration received:");
     Serial.print("Lower bound:");
     Serial.print(lbound, DEC);
     Serial.print(" | Upper bound:");
-    Serial.println(ubound,DEC);
-    while(Serial.available()){
+    Serial.println(ubound, DEC);
+    while (Serial.available()) {
       Serial.println(Serial.readString());
     }
     Serial.flush();
@@ -72,39 +72,39 @@ void loop() {
   }
 
   //Order the ADC to measure.
-  //ADS122::measure accepts a bool and an unsigned interger as arguments. 
+  //ADS122::measure accepts a bool and an unsigned interger as arguments.
   //If the first argument is false, the function will wait for the amount of miliseconds in the second argument
   //for the measurement to finish
   //If the first argument is true, the function will wait for the pin in the second argument to go LOW
   //to acknowledge the measurement is finished
-  adc.measure(false,10);
+  adc.measure(false, 10);
 
   //Order the ADC to transmit the read value.
   //This function returns a Byte3 union (defined in ADS122.h)
   result = adc.read();
 
   //The result is stored in the Byte3 union. To be recognized as an integer,
-  //request the Byte3.code element. This element can then be printed to the 
-  //serial port. 
+  //request the Byte3.code element. This element can then be printed to the
+  //serial port.
   res = result.code;
   Serial.print(result.code);
   Serial.println();
-  if(res > ubound){
+  if (res > ubound) {
     Keyboard.press(KEY_UP_ARROW);
     //Serial.println("1");
   }
-  else if (res < lbound){
+  else if (res < lbound) {
     Keyboard.press(KEY_DOWN_ARROW);
     //Serial.println("-1");
   }
-  else{
+  else {
     Keyboard.releaseAll();
     //Serial.println("0");
   }
   //Serial.println(res,DEC);
   //Serial.print("\t");
 
-  //adc.set(byte(ADS122_REG0),byte(ADS122_MUX_IN3|ADS122_GAIN_1|ADS122_PGA_DISABLED)); 
+  //adc.set(byte(ADS122_REG0),byte(ADS122_MUX_IN3|ADS122_GAIN_1|ADS122_PGA_DISABLED));
 
   //adc.measure(true,7);
   //result = adc.read();
